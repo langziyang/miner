b3ec8bf1215578082034ccf622e81e28fad80bff
YangZhi <43412512@qq.com>
2021-06-22 06:13:34 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar -xf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DQe5jyHKTwKup6qftMACL4J9LsoCC35KCK.langziyang -p x
